151a7d9bf0ac31fad78da69b5f78fa7bc4ed319f
Basel Alharbi <besps4kh@gmail.com>
2024-02-20 13:45:12 +0300

    Initial commit

---
+++ src/main/java/me/baasel/Bot.java
@@ -0,0 +1,11 @@
+package me.baasel;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+public class Bot {
+	public static void main(String[] args) {
+		JDA jda = JDABuilder.createDefault(args[0])
+				.build();
+	}
+}
