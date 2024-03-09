57f84a17f15052b7fde2ea06eb7d0496165712e0
Basel Alharbi <besps4kh@gmail.com>
2024-03-09 22:40:00 +0300

    Change activity status

--- src/main/java/me/baasel/Bot.java
+++ src/main/java/me/baasel/Bot.java
@@ -5,16 +5,18 @@ import com.jagrosh.jdautilities.command.CommandClientBuilder;
 import me.baasel.command.ColorCommand;
 import me.baasel.listener.GhostPingListener;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 
 public class Bot {
 	public static void main(String[] args) {
-		CommandClientBuilder commandClientBuilder = new CommandClientBuilder();
-		commandClientBuilder.setOwnerId("647887379981402118");
-		commandClientBuilder.addSlashCommands(
-				new ColorCommand()
-		);
-		CommandClient commandClient = commandClientBuilder.build();
+		CommandClient commandClient = new CommandClientBuilder()
+				.setActivity(Activity.watching("F15 members"))
+				.setOwnerId("647887379981402118")
+				.addSlashCommands(
+						new ColorCommand()
+				).build();
+
 		JDABuilder.createDefault(args[0])
 				.enableIntents(GatewayIntent.MESSAGE_CONTENT)
 				.addEventListeners(
